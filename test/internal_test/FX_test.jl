3a307745007abd9c3d1d27cfa3f5fa1d18aa3d53
Ryan Jadidi <rampbj@gmail.com>
2020-12-10 18:14:40 +1100

    Progressing with plotting.

--- test/internal_test/FX_test.jl
+++ test/internal_test/FX_test.jl
@@ -7,9 +7,12 @@ using Pipe: @pipe
 
 
 python_lib_path = normpath(abspath(@__FILE__), "../../../../../Python")
+result_path = normpath(abspath(@__FILE__), "../../../result/all_usd_jpy.csv")
+python_result_path = normpath(abspath(@__FILE__), "../../../result/python.csv")
 pushfirst!(PyVector(pyimport("sys")."path"), python_lib_path)
 MRP = pyimport("Market_regime_plot")
 pd = pyimport("pandas")
+np = pyimport("numpy")
 
 function test_dr_forex(;save_file=false, plt=false, dc_offset=[0.01,0.02])
 	data_file_path = normpath(abspath(@__FILE__), "../../../../../data/fx_usd_jpy/fx_usd_jpy_close_only/all.csv")
@@ -17,19 +20,26 @@ function test_dr_forex(;save_file=false, plt=false, dc_offset=[0.01,0.02])
 	df[!,:Timestamp] = parse.(DateTime, df.Timestamp, dateformat"yyyymmdd\ HHMMSS")
 	data = @pipe MarketRegime.init(df, dc_offset) |> MarketRegime.prepare(_...) |> MarketRegime.fit(_...)
 	if save_file
-		data_result_path = normpath(abspath(@__FILE__), "../../../result/all_usd_jpy.csv")
-		writing_csv = @task CSV.write(data_result_path, data)
+		writing_csv = @task CSV.write(result_path, data)
 		schedule(writing_csv)
 	end
 	if plt
 		wait(writing_csv)
-		df = pd.read_csv(data_result_path)
-		df.set_index("Timestamp", inplace=true)
-		df.index = pd.to_datetime(df.index)
-		MRP_init = MRP.Market_regime_plot(df, data_freq="m")
-		MRP_init.plot_market_regime()
+		plot_graph(result_path)
 	end
 	return data
 end
 
-test_dr_forex(save_file=true, plt=true)
+function plot_graph(result_path)
+	df = pd.read_csv(result_path)
+	df.set_index("Timestamp", inplace=true)
+	df.index = pd.to_datetime(df.index)
+	df_numberOfYears = np.unique(df.index.year)
+	for (_, year) in enumerate(df_numberOfYears)
+		data = get(df.loc, (string(year)))
+		MRP_init = MRP.Market_regime_plot(data, data_freq="m")
+		MRP_init.plot_market_regime()
+    end
+end
+# test_dr_forex(save_file=true, plt=true)
+plot_graph(result_path)
