acbdfee32e0b0d09c4132fdb8c01232a1535e892
Ryan Jadidi <rampbj@gmail.com>
2020-12-31 20:27:25 +1100

    making testing more streamlined.

--- test/internal_test/FX_test.jl
+++ test/internal_test/FX_test.jl
@@ -15,7 +15,7 @@ np = pyimport("numpy")
 data = normpath(abspath(@__FILE__), "../../../../../data/fx_usd_jpy/fx_usd_jpy_close_only/all.csv")
 result_folder = normpath(abspath(@__FILE__), "../../../result/")
 
-function test_dr_forex(df::DataFrame, result_path ;save_file=false, plt=false, dc_offset)
+function test_dr_forex(df::DataFrame, dc_offset::AbstractVector, result_path::String ;save_file=false, plt=false, plt_data_freq="d")
 	df[!,:Timestamp] = parse.(DateTime, df.Timestamp, dateformat"yyyymmdd\ HHMMSS")
 	data = @pipe MarketRegime.init(df, dc_offset) |> MarketRegime.prepare(_...) |> MarketRegime.fit(_...)
 	if save_file
@@ -26,16 +26,16 @@ function test_dr_forex(df::DataFrame, result_path ;save_file=false, plt=false, d
 		if save_file
 			wait(writing_csv)
 		end
-		plot_graph(result_path)
+		plot_graph(result_path, plt_data_freq)
 	end
 	return nothing
 end
 
-function plot_graph(result_path)
+function plot_graph(result_path::String, plt_data_freq::String)
 	df = pd.read_csv(result_path)
 	df.set_index("Timestamp", inplace=true)
 	df.index = pd.to_datetime(df.index)
-	MRP_main = MRP.Main(df, data_freq="d")
+	MRP_main = MRP.Main(df, data_freq=plt_data_freq)
 	MRP_main.plot_market_regime()
 end
 
@@ -44,10 +44,9 @@ function market_data_test()
 	data = DataFrame(yahoo(:SPY, YahooOpt(period1=t - Year(1), period2=t, interval="1d")))
 	data = convert(DataFrame, data)[!, [:timestamp, :Close ]]
 	result_path = normpath(abspath(@__FILE__), "../../../result/spy_one_year.csv")
-	test_dr_forex(data, result_path, save_file=true, plt=false, dc_offset=[0.1])
+	test_dr_forex(data, [0.1], result_path, save_file=true, plt=false)
 end
 df = CSV.read(data, DataFrame)
 result_path = result_folder * "usd_jpy_0_01.csv"
-test_dr_forex(df, result_path, save_file=true, plt=false, dc_offset=[0.01])
-# plot_graph(result_path)
+test_dr_forex(df,[0.01], result_path, save_file=true, plt=true, plt_data_freq="m")
 # market_data_test()
